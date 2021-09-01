233ded7a20acba17945efabf055e4050c2875db5
Milena V <milenavalenze@hotmail.com>
2021-09-01 20:48:38 -0300

    Primeira tela com um campo para login

---
+++ screens/Authenticantion/Authentication.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import "./styles.css";
+
+const Authentication = () => {
+  return (
+    <div className={"mahoe"}>
+      <h1>Insira seu login</h1>
+      <div>
+        <input/>
+      </div>
+    </div>
+  );
+};
+
+export default Authentication;
